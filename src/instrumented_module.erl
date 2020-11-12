884b48e8adb499c5da8a6e0510b44bea67b0fa72
Andre Fatton <afa@erl.io>
2020-11-12 18:05:40 +0100

    remove dup

--- src/instrumented_module.erl
+++ src/instrumented_module.erl
@@ -2,26 +2,12 @@
 
 -include_lib("opentelemetry_api/include/otel_tracer.hrl").
 
--export([span_with_delay_ms/1, span_with_sleep_ms/1]).
-
-
-span_with_delay_ms(Delay) ->
-Sampler = otel_sampler:setup(always_on, #{}),
-Tracer = opentelemetry:get_tracer(otel_example),
-io:format("Sampler: ~p~n,  Tracer: ~p~n", [Sampler, Tracer]),
-otel_tracer:with_span(Tracer, <<"span_with_delay/1">>, #{sampler => Sampler, is_recording => true}, 
-  fun(_SpanCtx) ->
-    SpanCtx = otel_tracer:current_span_ctx(),
-
-    otel_span:set_attribute(SpanCtx, <<"Delay">>, Delay),
-    timer:sleep(Delay)
-    end).
-
+-export([span_with_sleep_ms/1]).
 
 span_with_sleep_ms(Delay) ->
 Sampler = otel_sampler:setup(always_on, #{}),
 Tracer = opentelemetry:get_tracer(otel_example),
-io:format("Sampler: ~p~n,  Tracer: ~p~n", [Sampler, Tracer]),
+%io:format("Sampler: ~p~n,  Tracer: ~p~n", [Sampler, Tracer]),
 otel_tracer:with_span(Tracer, <<"span_with_sleep_ms/1">>, #{sampler => Sampler, is_recording => true}, 
   fun(SpanCtx) ->
     otel_span:set_attribute(SpanCtx, <<"Delay">>, Delay),
