a9659b293e4eb117322736f01eed1c1222793edf
Andre Fatton <afa@erl.io>
2020-11-13 15:14:02 +0100

    keep your things in order

--- src/otel_example_app.erl
+++ src/otel_example_app.erl
@@ -10,16 +10,19 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
+    application:load(opentelemetry),
 
-    {ok, _} = application:ensure_all_started(opentelemetry),
-    {ok, _} = application:ensure_all_started(opentelemetry_api),
-    application:set_env(opentelemetry, sampler, {always_on, #{}}),
-    application:set_env(opentelemetry, processors, [{otel_batch_processor, #{exporter => {otel_exporter_stdout, []},
-                                                                            exporting_timeout_ms => 100,
+    ok = application:set_env(opentelemetry, sampler, {always_on, #{}}),
+    ok = application:set_env(opentelemetry, processors, [{otel_batch_processor, #{exporter => {otel_exporter_stdout, []},
+                                                                            exporting_timeout_ms => 5000,
                                                                             scheduled_delay_ms => timer:seconds(5)}}]),
-    otel_batch_processor:set_exporter(otel_exporter_stdout, []),
-    true = opentelemetry:register_application_tracer(otel_example), 
 
+    {ok, _} = application:ensure_all_started(opentelemetry_api),
+    {ok, _} = application:ensure_all_started(opentelemetry),
+
+    ok = otel_batch_processor:set_exporter(otel_exporter_stdout, []),
+    true = opentelemetry:register_application_tracer(otel_example), 
+    
 
     % otel_batch_processor:set_exporter(opentelemetry_exporter, #{protocol => http_protobuf,
     %                                                          endpoints => [
