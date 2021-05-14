4f24216eab600e3bee4891ad8d8ac4e46bbdd93f
Andre Fatton <afa@erl.io>
2021-05-14 09:20:48 +0200

    example

--- README.md
+++ README.md
@@ -7,3 +7,19 @@ Build
 -----
 
     $ rebar3 compile
+    $ erl -pa _build/default/lib/*/ebin -pa _build/default/lib/*/apps/*/ebin
+    Eshell V11.1.4  (abort with ^G)
+    1> application:start(otel_example).
+    ok
+    2> instrumented_module:span_with_sleep_ms_2(1000).
+    *SPANS FOR DEBUG*
+    ok
+    3> *SPANS FOR DEBUG*
+    {span,87313737888284145423376002069858722709,16567822515868545333,undefined,
+      undefined,<<"instrumented_module:span_with_sleep2">>,'INTERNAL',
+      -576460732348564606,-576460731344815009,
+      [{<<"delay">>,1000}],
+      [],[],undefined,1,false,
+      {instrumentation_library,<<"otel_example">>,<<"0.1.0">>}}
+
+
--- src/instrumented_module.erl
+++ src/instrumented_module.erl
@@ -1,9 +1,12 @@
 -module(instrumented_module).
 
 -include_lib("opentelemetry_api/include/otel_tracer.hrl").
+%-include_lib("opentelemetry_api/include/opentelemetry.hrl").
 
--export([span_with_sleep_ms/1]).
 
+-export([span_with_sleep_ms/1, span_with_sleep_ms_2/1]).
+
+% Approach 1: with Tracer and Sampler
 span_with_sleep_ms(Delay) ->
 Sampler = otel_sampler:setup(always_on, #{}),
 Tracer = opentelemetry:get_tracer(otel_example),
@@ -13,3 +16,10 @@ otel_tracer:with_span(Tracer, <<"span_with_sleep_ms/1">>, #{sampler => Sampler, 
     otel_span:set_attribute(SpanCtx, <<"Delay">>, Delay),
     timer:sleep(Delay)
     end).
+% Approach 2: with ?with_span macro
+span_with_sleep_ms_2(Delay) ->
+  ?with_span(<<"instrumented_module:span_with_sleep2">>, #{}, 
+  fun(_SpanCtx) ->
+  ?set_attribute(<<"delay">>, Delay),
+        timer:sleep(Delay)
+        end).
